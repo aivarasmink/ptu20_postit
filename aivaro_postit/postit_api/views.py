be94eaf5ec19e20b6f5e552ba67e3c4158f5ab57
aivarasmink <aivarasmink@gmail.com>
2024-03-05 09:09:04 +0200

    nauja diena nauje tema

--- aivaro_postit/postit_api/views.py
+++ aivaro_postit/postit_api/views.py
@@ -10,3 +10,9 @@ class PostList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = models.Post.objects.all()
+    serializer_class = serializers.PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    
